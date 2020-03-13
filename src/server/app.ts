41705177c5d1088004b6d133e6fb61ef74c9badc
Jerko Steiner <jerko.steiner@gmail.com>
2020-03-13 21:40:53 +0100

    Add redis store
    Tested locally with docker-compose and two instances of peer-calls
    running on different ports.

--- src/server/app.ts
+++ src/server/app.ts
@@ -9,9 +9,10 @@ import SocketIO from 'socket.io'
 import call from './routes/call'
 import index from './routes/index'
 import ejs from 'ejs'
-import { MemoryStore } from './store'
-// import Redis from 'ioredis'
-// import redisAdapter from 'socket.io-redis'
+// import { MemoryStore } from './store'
+import Redis from 'ioredis'
+import redisAdapter from 'socket.io-redis'
+import { RedisStore } from './store/redis'
 
 const debug = _debug('peercalls')
 const logRequest = _debug('peercalls:requests')
@@ -24,19 +25,19 @@ debug(`WebSocket URL: ${SOCKET_URL}`)
 const app = express()
 const server = createServer(config, app)
 export const io = SocketIO(server, { path: SOCKET_URL })
-// const pubClient = new Redis({
-//   host: '127.0.0.1',
-//   port: 6379,
-// })
-// const subClient = new Redis({
-//   host: '127.0.0.1',
-//   port: 6379,
-// })
-// io.adapter(redisAdapter({
-//   key: 'peercalls',
-//   pubClient,
-//   subClient,
-// }))
+const pubClient = new Redis({
+  host: '127.0.0.1',
+  port: 6379,
+})
+const subClient = new Redis({
+  host: '127.0.0.1',
+  port: 6379,
+})
+io.adapter(redisAdapter({
+  key: 'peercalls',
+  pubClient,
+  subClient,
+}))
 
 app.set('x-powered-by', false)
 app.locals.version = require('../../package.json').version
@@ -65,8 +66,8 @@ router.use('/', index)
 app.use(BASE_URL, router)
 
 const stores = {
-  socketIdByUserId: new MemoryStore(),
-  userIdBySocketId: new MemoryStore(),
+  socketIdByUserId: new RedisStore(pubClient, 'peercalls:socketIdByUserId'),
+  userIdBySocketId: new RedisStore(pubClient, 'peercalls:userIdBySocketId'),
 }
 io.on('connection', socket => handleSocket(socket, io, stores))
 
