3393a16d68320918fa8924fd4d2607bf6991de0b
Jerko Steiner <jerko.steiner@gmail.com>
2020-03-30 12:58:33 +0200

    Add debug for ICEServers

--- src/client/window.ts
+++ src/client/window.ts
@@ -1,15 +1,20 @@
+import _debug from 'debug'
+const debug = _debug('peercalls')
+
 export const createObjectURL = (object: unknown) =>
   window.URL.createObjectURL(object)
 export const revokeObjectURL = (url: string) => window.URL.revokeObjectURL(url)
 
 export const valueOf = (id: string) => {
   const el = window.document.getElementById(id) as HTMLInputElement
-  return el && el.value
+  return el ? el.value : null
 }
 
-export const baseUrl = valueOf('baseUrl')
-export const callId = valueOf('callId')
-export const userId = valueOf('userId')
+export const baseUrl = valueOf('baseUrl')!
+export const callId = valueOf('callId')!
+export const userId = valueOf('userId')!
 export const iceServers = JSON.parse(valueOf('iceServers')!)
+
+debug('Using ice servers: %s', iceServers)
 
 export const MediaStream = window.MediaStream
