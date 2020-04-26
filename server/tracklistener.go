877169b61350980bb95f3595baca4349da6d34d4
Jerko Steiner <jerko.steiner@gmail.com>
2020-04-26 19:48:33 +0200

    Only send PLI when requested

--- server/tracklistener.go
+++ server/tracklistener.go
@@ -90,12 +90,47 @@ func (p *trackListener) GetTracksMetadata() (metadata []TrackMetadata) {
 	return
 }
 
-func (p *trackListener) AddTrack(sourceClientID string, track *webrtc.Track) error {
+func (p *trackListener) AddTrack(sourcePC *webrtc.PeerConnection, sourceClientID string, track *webrtc.Track) error {
 	p.localTracksMu.Lock()
 	defer p.localTracksMu.Unlock()
 
 	p.log.Printf("[%s] peer.AddTrack: add sendonly transceiver for track: %s", p.clientID, track.ID())
 	rtpSender, err := p.peerConnection.AddTrack(track)
+
+	go func() {
+		for {
+			rtcps, err := rtpSender.ReadRTCP()
+			if err != nil {
+				p.log.Printf("[%s] Error reading rtcp for sender track: %d: %s",
+					p.clientID,
+					track.SSRC(),
+					err,
+				)
+				break
+			}
+			for _, pkt := range rtcps {
+				switch pkt.(type) {
+				case *rtcp.PictureLossIndication:
+					err := sourcePC.WriteRTCP(
+						[]rtcp.Packet{
+							&rtcp.PictureLossIndication{
+								MediaSSRC: track.SSRC(),
+							},
+						},
+					)
+					if err != nil {
+						p.log.Printf("[%s] Error sending rtcp PLI for local track: %d: %s",
+							p.clientID,
+							track.SSRC(),
+							err,
+						)
+					}
+					// default:
+					//   p.log.Printf("[%s] Got rtcp pkt for track: %d (%T)", p.clientID, track.SSRC(), pkt)
+				}
+			}
+		}
+	}()
 	// t, err := p.peerConnection.AddTransceiverFromTrack(
 	// 	track,
 	// 	webrtc.RtpTransceiverInit{
@@ -143,7 +178,7 @@ func (p *trackListener) RemoveTrack(track *webrtc.Track) error {
 func (p *trackListener) handleTrack(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
 	p.log.Printf("[%s] peer.handleTrack (id: %s, label: %s, type: %s, ssrc: %d)",
 		p.clientID, remoteTrack.ID(), remoteTrack.Label(), remoteTrack.Kind(), remoteTrack.SSRC())
-	localTrack, err := p.startCopyingTrack(remoteTrack)
+	localTrack, err := p.startCopyingTrack(remoteTrack, receiver)
 	if err != nil {
 		p.log.Printf("Error copying remote track: %s", err)
 		return
@@ -165,7 +200,7 @@ func (p *trackListener) Tracks() []*webrtc.Track {
 	return p.localTracks
 }
 
-func (p *trackListener) startCopyingTrack(remoteTrack *webrtc.Track) (*webrtc.Track, error) {
+func (p *trackListener) startCopyingTrack(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) (*webrtc.Track, error) {
 	remoteTrackID := remoteTrack.ID()
 	if remoteTrackID == "" {
 		remoteTrackID = NewUUIDBase62()
@@ -194,34 +229,8 @@ func (p *trackListener) startCopyingTrack(remoteTrack *webrtc.Track) (*webrtc.Tr
 	// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
 	// This can be less wasteful by processing incoming RTCP events, then we would emit a NACK/PLI when a viewer requests it
 
-	ticker := time.NewTicker(rtcpPLIInterval)
-	go func() {
-		writeRTCP := func() {
-			err := p.peerConnection.WriteRTCP(
-				[]rtcp.Packet{
-					&rtcp.PictureLossIndication{
-						MediaSSRC: ssrc,
-					},
-				},
-			)
-			if err != nil {
-				p.log.Printf("[%s] Error sending rtcp PLI for local track: %s: %s",
-					p.clientID,
-					localTrackID,
-					err,
-				)
-			}
-		}
-
-		writeRTCP()
-		for range ticker.C {
-			writeRTCP()
-		}
-	}()
-
 	go func() {
 		defer p.sendTrackEvent(TrackEvent{p.clientID, localTrack, TrackEventTypeRemove})
-		defer ticker.Stop()
 		rtpBuf := make([]byte, 1400)
 		for {
 			i, err := remoteTrack.Read(rtpBuf)
