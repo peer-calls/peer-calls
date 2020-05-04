94561109a8d7c18bc289b9d2890f372d5229600d
Jerko Steiner <jerko.steiner@gmail.com>
2020-05-04 13:14:31 +0200

    Fix source maps for ts files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,17 @@
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   ...common,
-  // plugins: [
-  //   new BundleAnalyzerPlugin(),
-  // ],
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: null,
+      exclude: [/node_modules/],
+      test: /\.tsx?$/,
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
   mode: 'development',
 }
