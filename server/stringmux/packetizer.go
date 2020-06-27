9b8eea96c097c4c9860ce9ed4842e40de51b66a2
Jerko Steiner <jerko.steiner@gmail.com>
2020-06-27 09:56:50 +0200

    Reduce stringmux header to only 2 bytes

--- server/stringmux/packetizer.go
+++ server/stringmux/packetizer.go
@@ -1,9 +1,12 @@
 package stringmux
 
 import (
-	"encoding/binary"
 	"fmt"
-	"math"
+)
+
+const (
+	StringMuxByte  uint8 = 0b11001000
+	MaxLenStreamID int   = 0xFF
 )
 
 func Marshal(streamID string, data []byte) ([]byte, error) {
@@ -11,7 +14,7 @@ func Marshal(streamID string, data []byte) ([]byte, error) {
 	 *         0                   1                   2                   3
 	 *         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 	 *        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	 * header |        P      |       C       |         Stream ID len         |
+	 * header | StringMuxByte | StreamID len  |   StreamID ...                |
 	 *        +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
 	 *        |                           StreamID                            |
 	 *        |                              ...                              |
@@ -22,22 +25,19 @@ func Marshal(streamID string, data []byte) ([]byte, error) {
 	 */
 
 	lenStreamID := len(streamID)
-	if lenStreamID > math.MaxUint16 {
+	if lenStreamID > MaxLenStreamID {
 		return nil, fmt.Errorf("StreamID too large")
 	}
 
-	result := make([]byte, 4+lenStreamID+len(data))
+	result := make([]byte, 2+lenStreamID+len(data))
 
 	offset := 0
 
-	result[offset] = 'P'
+	result[offset] = StringMuxByte
 	offset++
 
-	result[offset] = 'C'
+	result[offset] = uint8(lenStreamID)
 	offset++
-
-	binary.BigEndian.PutUint16(result[offset:offset+2], uint16(lenStreamID))
-	offset += 2
 
 	copy(result[offset:offset+lenStreamID], streamID)
 	offset += lenStreamID
@@ -48,26 +48,26 @@ func Marshal(streamID string, data []byte) ([]byte, error) {
 }
 
 func Unmarshal(data []byte) (string, []byte, error) {
-	if len(data) < 4 {
+	if len(data) < 2 {
 		return "", nil, fmt.Errorf("Header is too short")
 	}
 
 	offset := 0
 
-	if string(data[0:2]) != "PC" {
-		return "", nil, fmt.Errorf("First two bytes should be 'PC'")
+	if data[0] != StringMuxByte {
+		return "", nil, fmt.Errorf("First byte should be %b", StringMuxByte)
 	}
-	offset += 2
+	offset++
 
-	lenStreamID := binary.BigEndian.Uint16(data[offset : offset+2])
-	offset += 2
+	lenStreamID := int(data[offset])
+	offset++
 
-	if len(data) < offset+int(lenStreamID) {
+	if len(data) < offset+lenStreamID {
 		return "", nil, fmt.Errorf("StreamID length mismatch")
 	}
 
-	streamID := string(data[offset : offset+int(lenStreamID)])
-	offset += int(lenStreamID)
+	streamID := string(data[offset : offset+lenStreamID])
+	offset += lenStreamID
 
 	result := data[offset:]
 
--- server/stringmux/packetizer_test.go
+++ server/stringmux/packetizer_test.go
@@ -13,7 +13,7 @@ func TestMarshalUnmarshal(t *testing.T) {
 	payload, err := Marshal("mystream", data)
 	require.NoError(t, err)
 
-	assert.Equal(t, []byte{'P', 'C', 0, 8}, payload[0:4])
+	assert.Equal(t, []byte{StringMuxByte, 8}, payload[0:2])
 
 	streamID, data2, err := Unmarshal(payload)
 	require.NoError(t, err)
@@ -22,28 +22,28 @@ func TestMarshalUnmarshal(t *testing.T) {
 }
 
 func TestMarshal_StreamIDTooLong(t *testing.T) {
-	streamID := string(make([]byte, 65535+1))
+	streamID := string(make([]byte, 0xFF+1))
 	_, err := Marshal(streamID, []byte{1, 2, 3})
 	require.EqualError(t, err, "StreamID too large")
 }
 
 func TestUnmarshal_InvalidFirstTwoBytes(t *testing.T) {
-	_, _, err := Unmarshal([]byte{0, 0, 0, 3, 's', 'i', 'd', 1, 2, 3})
-	require.EqualError(t, err, "First two bytes should be 'PC'")
+	_, _, err := Unmarshal([]byte{123, 2, 'H', 'I', 1, 1})
+	require.EqualError(t, err, "First byte should be 11001000")
 }
 
 func TestUnmarshal_InvalidHeader(t *testing.T) {
-	_, _, err := Unmarshal([]byte{0, 0, 0})
+	_, _, err := Unmarshal([]byte{StringMuxByte})
 	require.EqualError(t, err, "Header is too short")
 }
 
 func TestUnmarshal_LengthMismatch(t *testing.T) {
-	_, _, err := Unmarshal([]byte{'P', 'C', 0, 3, 'i', 'd'})
+	_, _, err := Unmarshal([]byte{StringMuxByte, 3, 'T'})
 	require.EqualError(t, err, "StreamID length mismatch")
 }
 
 func TestUnmarshal_OK(t *testing.T) {
-	streamID, data, err := Unmarshal([]byte{'P', 'C', 0, 2, 'i', 'd', 'd', 'a', 't', 'a'})
+	streamID, data, err := Unmarshal([]byte{StringMuxByte, 2, 'i', 'd', 'd', 'a', 't', 'a'})
 	require.NoError(t, err)
 
 	assert.Equal(t, "id", streamID)
