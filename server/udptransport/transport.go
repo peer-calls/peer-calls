b1762e174e3366fe6ce7c619ce0d6cb03e034a54
Jerko Steiner <jerko.steiner@gmail.com>
2021-01-01 10:15:21 +0100

    Add FIXME to udptransport.Transport.Close

--- server/udptransport/transport.go
+++ server/udptransport/transport.go
@@ -21,6 +21,21 @@ func (t *Transport) Close() error {
 	errs := multierr.New()
 
 	errs.Add(errors.Annotate(t.Transport.Close(), "close transport"))
+
+	// FIXME closing the association is not as trivial as calling close. There
+	// are two ways of closing a SCTP association: using Abort and Shutdown,
+	// neither of which are currently implemented in pion/sctp. I have a MR
+	// to address the Shutdown, but it hasn't yet been merged:
+	//
+	// https://github.com/pion/sctp/pull/176.
+	//
+	// Until then, we most likely need a better mechanism for supporting
+	// shutdowns.
+	//
+	// Core issue: NodeManager will close the server association as soon as there
+	// are no WebRTC connections left in the room. Eventually this Close method
+	// will be called and the other end of the connection will be left hanging
+	// because the Shutdown/Abort is not implemented.
 	errs.Add(errors.Annotate(t.association.Close(), "close association"))
 	errs.Add(errors.Annotate(t.stringMux.Close(), "close string mux"))
 
